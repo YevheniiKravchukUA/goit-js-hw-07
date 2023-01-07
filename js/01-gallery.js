70613a974da300b4e2e0c411852cb3a772e2b879
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-01-07 20:06:07 +0100

    do task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+
+galleryItems.forEach(({ preview, original, description }, idx, arr) => {
+  const markupString = `<div class="gallery__item">
+  <a class="gallery__link" src=${original}>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+  galleryRef.insertAdjacentHTML("beforeend", markupString);
+});
+
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault;
+
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  const showedImage = basicLightbox.create(
+    `
+		    <img width="1400" height="900" src="${e.target.dataset.source}">
+	    `
+  );
+
+  showedImage.show(() => {
+    window.addEventListener("keydown", closeOnEscapeButton);
+  });
+
+  function closeOnEscapeButton(e) {
+    if (e.code === "Escape") {
+      console.log(e.code);
+
+      showedImage.close(() => {
+        window.removeEventListener("keydown", closeOnEscapeButton);
+      });
+    }
+  }
+});
